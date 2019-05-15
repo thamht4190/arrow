adf559f22bdfca15e65728dc26c0358e6dd7a02d
Ha Thi Tham <thamht01188@gmail.com>
2019-05-15 09:45:40 +0700

    remove change in crypto.h/.cc

--- cpp/src/parquet/util/crypto.h
+++ cpp/src/parquet/util/crypto.h
@@ -22,103 +22,25 @@
 
 #include "parquet/properties.h"
 #include "parquet/types.h"
-#include <openssl/evp.h>
 
-using parquet::ParquetCipher;
+using parquet::Encryption;
+using parquet::EncryptionProperties;
 
 namespace parquet_encryption {
 
-constexpr int GCMTagLength = 16;
-constexpr int NonceLength = 12;
+int Encrypt(Encryption::type alg_id, bool metadata, const uint8_t* plaintext,
+            int plaintext_len, uint8_t* key, int key_len, uint8_t* aad, int aad_len,
+            uint8_t* ciphertext);
 
-// Module types
-const int8_t Footer = 0;
-const int8_t ColumnMetaData = 1;
-const int8_t DataPage = 2;
-const int8_t DictionaryPage = 3;
-const int8_t DataPageHeader = 4;
-const int8_t DictionaryPageHeader = 5;
-const int8_t ColumnIndex = 6;
-const int8_t OffsetIndex = 7;
+int Encrypt(std::shared_ptr<EncryptionProperties> encryption_props, bool metadata,
+            const uint8_t* plaintext, int plaintext_len, uint8_t* ciphertext);
 
+int Decrypt(Encryption::type alg_id, bool metadata, const uint8_t* ciphertext,
+            int ciphertext_len, uint8_t* key, int key_len, uint8_t* aad, int aad_len,
+            uint8_t* plaintext);
 
-class AesEncryptor {
- public:
-  // Can serve one key length only. Possible values: 16, 24, 32 bytes.
-  AesEncryptor(ParquetCipher::type alg_id, int key_len, bool metadata);
-
-  // Size different between plaintext and ciphertext, for this cipher.
-  int CiphertextSizeDelta();
-
-  // Key length is passed only for validation. If different from value in
-  // constructor, exception will be thrown.
-  int Encrypt(const uint8_t* plaintext, int plaintext_len, uint8_t* key, int key_len, 
-              uint8_t* aad, int aad_len, uint8_t* ciphertext);
-
-  int SignedFooterEncrypt(const uint8_t* footer, int footer_len, uint8_t* key, int key_len,
-                          uint8_t* aad, int aad_len, uint8_t* nonce, uint8_t* encrypted_footer);
-  
-  ~AesEncryptor() {
-    if (nullptr != ctx_) {
-      EVP_CIPHER_CTX_free(ctx_);
-    }
-  }
-
- private:
-  EVP_CIPHER_CTX* ctx_;
-  int aes_mode_;
-  int key_length_;
-  int ciphertext_size_delta_;
-
-  int gcm_encrypt(const uint8_t* plaintext, int plaintext_len, uint8_t* key, int key_len,
-                  uint8_t* nonce, uint8_t* aad, int aad_len, uint8_t* ciphertext);
-
-  int ctr_encrypt(const uint8_t* plaintext, int plaintext_len, uint8_t* key, int key_len,
-                  uint8_t* nonce, uint8_t* ciphertext);
-};
-
-class AesDecryptor {
- public:
-  // Can serve one key length only. Possible values: 16, 24, 32 bytes.
-  AesDecryptor(ParquetCipher::type alg_id, int key_len, bool metadata);
-
-  // Size different between plaintext and ciphertext, for this cipher.
-  int CiphertextSizeDelta();
-
-  // Key length is passed only for validation. If different from value in
-  // constructor, exception will be thrown.
-  int Decrypt(const uint8_t* ciphertext, int ciphertext_len, uint8_t* key, int key_len, 
-              uint8_t* aad, int aad_len, uint8_t* plaintext);
-  
-  ~AesDecryptor() {
-    if (nullptr != ctx_) {
-      EVP_CIPHER_CTX_free(ctx_);
-    }
-  }
-
- private:
-  EVP_CIPHER_CTX* ctx_;
-  int aes_mode_;
-  int key_length_;
-  int ciphertext_size_delta_;
-
-  int gcm_decrypt(const uint8_t* ciphertext, int ciphertext_len, uint8_t* key, int key_len,
-                  uint8_t* aad, int aad_len, uint8_t* plaintext);
-
-  int ctr_decrypt(const uint8_t* ciphertext, int ciphertext_len, uint8_t* key, int key_len,
-                  uint8_t* plaintext);
-};
-
-
-std::string createModuleAAD(const std::string& fileAAD, int8_t module_type,
-			    int16_t row_group_ordinal, int16_t column_ordinal,
-			    int16_t page_ordinal);
-
-std::string createFooterAAD(const std::string& aad_prefix_bytes);
-
-// Update last two bytes of page (or page header) module AAD
-void quickUpdatePageAAD(const std::string &AAD, int16_t new_page_ordinal);
-
+int Decrypt(std::shared_ptr<EncryptionProperties> encryption_props, bool metadata,
+            const uint8_t* ciphertext, int ciphertext_len, uint8_t* plaintext);
 }  // namespace parquet_encryption
 
 #endif  // PARQUET_UTIL_CRYPTO_H
