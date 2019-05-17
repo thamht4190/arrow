1c55041016725831254db508bf6e348533150586
Ha Thi Tham <thamht01188@gmail.com>
2019-05-17 17:29:11 +0700

    fix build issue on MacOS

--- cpp/src/parquet/column_writer.cc
+++ cpp/src/parquet/column_writer.cc
@@ -190,8 +190,8 @@ class SerializedPageWriter : public PageWriter {
           data_encryptor_->file_aad(), parquet_encryption::DictionaryPage,
           row_group_ordinal_, column_ordinal_, (int16_t)-1));
 
-      encrypted_data_buffer->Resize(data_encryptor_->CiphertextSizeDelta() +
-                                    output_data_len);
+      PARQUET_THROW_NOT_OK(encrypted_data_buffer->Resize(
+          data_encryptor_->CiphertextSizeDelta() + output_data_len));
       output_data_len = data_encryptor_->Encrypt(compressed_data->data(), output_data_len,
                                                  encrypted_data_buffer->mutable_data());
       output_data_buffer = encrypted_data_buffer->data();
@@ -277,8 +277,8 @@ class SerializedPageWriter : public PageWriter {
     if (data_encryptor_.get()) {
       parquet_encryption::quickUpdatePageAAD(data_pageAAD_, page_ordinal_);
       data_encryptor_->update_aad(data_pageAAD_);
-      encrypted_data_buffer->Resize(data_encryptor_->CiphertextSizeDelta() +
-                                    output_data_len);
+      PARQUET_THROW_NOT_OK(encrypted_data_buffer->Resize(
+          data_encryptor_->CiphertextSizeDelta() + output_data_len));
       output_data_len = data_encryptor_->Encrypt(compressed_data->data(), output_data_len,
                                                  encrypted_data_buffer->mutable_data());
       output_data_buffer = encrypted_data_buffer->data();
